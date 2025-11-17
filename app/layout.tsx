fcabbdb4aca3294421715dea9488db2b73da1bab
Mwangimark <markcosmars2000@gmail.com>
2025-11-17 08:48:16 +0300

    changed favicon icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,30 +7,29 @@ interface RootLayoutProps {
   children: ReactNode;
 }
 
-export const metadata = {
-  title: "UltraQuad IT Consulting",
-  description: "Building Intelligent Solutions for the Future",
-};
-
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <head>
         <title>UltraQuad IT Consulting</title>
+        
         <meta
           name="description"
           content="Building Intelligent Solutions for the Future"
         />
+
         <meta
           name="google-site-verification"
           content="LyjzR2V-4NCRTTiafLgeg-6kYYlQFUAoPuVEZfEzeZM"
         />
         <meta name="keyword" content="ultraquad,ultraquad company,ultraquad consultancy,IT consulting companies in kenya, Cybersecurity solutions,AI Consulting,Fullstack Services" />
-        
+
       </head>
-      <Header/>
+
+
+      <Header />
       <body className="font-sans antialiased">{children}</body>
-      <Footer/>
+      <Footer />
     </html>
   );
 }
